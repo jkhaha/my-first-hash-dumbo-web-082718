c80327209f81fa6b1b98de91e6348468e610fce8
Jackie Ha <jha2@wellesley.edu>
2018-08-21 01:31:15 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  dictionary = {"apple" => "healthy fruit", "ice cream" => "honestly overrated", "chips" => "amazing}"
 end
 
 
