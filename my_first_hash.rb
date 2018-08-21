001bd2624c37c4ce464ccadb698dfbcee0cccb93
Jackie Ha <jha2@wellesley.edu>
2018-08-21 01:35:45 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,8 +16,9 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
+
+shipping_manifest[]
 
 def adding
   shipping_manifest = {
